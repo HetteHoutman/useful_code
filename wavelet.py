8da41b0649ea1743ac5a4ec75554adb07213e17d
HetteHoutman <h.houtman@hotmail.com>
2024-02-26 18:16:25 +0000

    cone of influence

--- wavelet.py
+++ wavelet.py
@@ -17,8 +17,9 @@ def max_lambda_theta(power_spectrum, lambdas, thetas):
 
     """
     posvect_pspec_max = argmax_lastNaxes(power_spectrum, 2)
-    dom_lambda = lambdas[np.moveaxis(posvect_pspec_max, 0, -1)[..., 0]]
-    dom_theta = thetas[np.moveaxis(posvect_pspec_max, 0, -1)[..., 1]]
+    posvect_pspec_max = np.moveaxis(posvect_pspec_max, 0, -1)
+    dom_lambda = np.ma.masked_where(power_spectrum.mask.all((-2,-1)), lambdas[posvect_pspec_max[..., 0]])
+    dom_theta = np.ma.masked_where(power_spectrum.mask.all((-2,-1)), thetas[posvect_pspec_max[..., 1]])
 
     return dom_lambda, dom_theta
 
@@ -89,3 +90,19 @@ def find_polar_max_and_error(polar_array, lambdas, thetas, height=0.5):
 
 def angle_error(angle1, angle2):
     return (angle2- angle1) - np.round((angle2 - angle1) / 180) * 180
+
+
+def _indices_array_generic(m, n):
+    r0 = np.arange(m)  # Or r0,r1 = np.ogrid[:m,:n], out[:,:,0] = r0
+    r1 = np.arange(n)
+    out = np.empty((m, n, 2), dtype=int)
+    out[:, :, 0] = r0[:, None]
+    out[:, :, 1] = r1
+    return out
+
+
+def cone_of_influence_mask(pspec, efold_dist, pixels_per_km):
+    idxs = _indices_array_generic(*pspec.shape[:2])
+    masks = np.array([(np.minimum(idxs[:,:,0], idxs[:,:,0][::-1]) / pixels_per_km < s) |
+                      (np.minimum(idxs[:,:,1], idxs[:,:,1][:, ::-1]) / pixels_per_km < s) for s in efold_dist])
+    return np.repeat(np.moveaxis(masks, 0, -1)[:, :, :, np.newaxis], pspec.shape[-1], axis=3)
